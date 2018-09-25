d70b7aa435eec92bd92f41ea1fb5f0b6c05972f6
rednithin <reddy.nithinpg@live.com>
2018-09-25 16:30:19 +0530

    Modified Print Script.

--- Print.sh
+++ Print.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-cd 1
 for file in `ls`
 do
-    echo $file
-    lpr -o page-left=70 -o page-right=35 -o page-bottom=17 -p $file
+    if [ "$file" != Print.sh ] ; then
+        echo $file
+        lpr -o page-left=55 -o page-right=25 -o page-bottom=17 -p $file
+    fi
 done
 
